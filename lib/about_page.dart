a15211db562dc7ac53d1525fa1f3e100ff9241a2
Lewis Ryan <lr9609914@gmail.com>
2025-11-08 16:53:22 +0000

    move header into its own widget

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:union_shop/header.dart';
 
 class AboutPage extends StatelessWidget {
   const AboutPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+        body: Column(
+      children: [
+        Header(),
+        Center(
+          child: Center(
+              child: Column(children: [Text('About Us'), Text('Placeholder')])),
+        )
+      ],
+    ));
   }
 }
