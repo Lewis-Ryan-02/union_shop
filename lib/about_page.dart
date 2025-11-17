b927fc3b55867c49a124c3d8b61fb2632d1357e2
Lewis Ryan <lr9609914@gmail.com>
2025-11-17 16:25:23 +0000

    replace placeholder text in the about page

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -11,10 +11,34 @@ class AboutPage extends StatelessWidget {
         body: Column(
       children: [
         const Header(),
-        const Center(
-          child: Center(
-              child: Column(children: [Text('About Us'), Text('Placeholder')])),
-        ),
+          const SizedBox(
+            width: 600,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                  Center(child: Text('About Us',style: TextStyle(fontWeight: FontWeight.bold),)),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 8.0,top: 8.0),
+                  child: Text('Welcome to the Union Shop!'),
+                ),
+                Text("We're dedicated to giving you the very best University branded products, with a range of clothing and merchandise avaliable to shop all year round! We even offer an exclusive"),
+                Text('personalisation service!',style: TextStyle(decoration: TextDecoration.underline)),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 8.0,top: 8.0),
+                  child: Text("All online purchases are avalible for dilivery or instore collection!"),
+                ),
+                Text("We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don't hesitate to contact us at hello@upsu.net."),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 8.0,top: 8.0),
+                  child: Text("Happy shopping!"),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 8.0,top: 8.0),
+                  child: Text("The Union Shop & Reception Team"),
+                ),
+                ]),
+          ),
+        const Spacer(),
         Footer(),
       ],
     ));
